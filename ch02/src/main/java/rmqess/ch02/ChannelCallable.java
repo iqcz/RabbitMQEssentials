6513b384ab292a4d86fdb263176701c329e0cd59
Phideon <a0z0to9@outlook.com>
2018-06-06 23:34:47 +0800

    rabbitmq

---
+++ ch02/src/main/java/rmqess/ch02/ChannelCallable.java
@@ -0,0 +1,12 @@
+
+package rmqess.ch02;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+
+public interface ChannelCallable<T> {
+    String getDescription();
+
+    T call(Channel channel) throws IOException;
+}
