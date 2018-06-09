8467b3d8ddca1acfcfadece036f922e5bc1600d9
Phideon <a0z0to9@outlook.com>
2018-06-09 17:48:23 +0800

    remove Duplicated method.

--- ch02/src/main/java/offical/workqueues/NewTask.java
+++ ch02/src/main/java/offical/workqueues/NewTask.java
@@ -31,21 +31,10 @@ public class NewTask {
     }
 
     private static String getMessage(String[] strings) {
-        if (strings.length < 1) return "Hello World!";
-
-        // return joinStrings(strings, " ");
-        return Joiner.on(" ").join(strings);
-    }
-
-    private static String joinStrings(String[] strings, String delimiter) {
-        int length = strings.length;
-        if (length == 0) return "";
-
-        StringBuilder words = new StringBuilder(strings[0]);
-        for (int i = 1; i < length; i++) {
-            words.append(delimiter).append(strings[i]);
+        if (strings.length < 1) {
+            return "Hello World!";
         }
 
-        return words.toString();
+        return Joiner.on(" ").join(strings);
     }
 }
